3e75f2d142774a19b26ef8a0485097da7086270b
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-05-29 10:44:25 -0300

    Fix image optimization endpoint and add Supabase domain to allowed list

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,11 @@ const config = {
         sizes: [640, 828, 1200, 1920],
         formats: ['image/webp'],
         minimumCacheTTL: 300,
-        domains: ['http2.mlstatic.com', 'placeholder.supabase.co']
+        domains: [
+          'http2.mlstatic.com',
+          'placeholder.supabase.co',
+          'ijbbwmtperruvunlyxzp.supabase.co'
+        ]
       }
     }),
 
