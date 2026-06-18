87fcac2e25377769c9ed265b3afbfade767c0692
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-06-18 08:57:34 -0300

    Remove old fragmented migration files

--- supabase/migrations/20260530000002_add_delivery_day_to_suppliers.sql
+++
@@ -1,5 +0,0 @@
-SET search_path = public, extensions;
-
-ALTER TABLE public.suppliers
-ADD COLUMN delivery_day INTEGER NOT NULL DEFAULT 0 CHECK (delivery_day BETWEEN 0 AND 6);
--- 0 = Sunday, 1 = Monday, ..., 6 = Saturday
